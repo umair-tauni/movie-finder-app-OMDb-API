b2cf564765bbabb63a321233464d56f32cbcb751
umair-tauni <engrumayr@gmail.com>
2024-11-03 17:09:54 +0500

    Add Customize Print statements

--- movieFinder.py
+++ movieFinder.py
@@ -11,8 +11,15 @@ class MovieDescription:
         try:
             url = f'http://www.omdbapi.com/?t={title}&apikey={self.api_key}'
             response = requests.get(url)
-            print(response.status_code)
-            print(response.json())
+            # print(response.status_code)
+            # print(response.json())
+            data = response.json()
+            if response.status_code == 200:
+                print('Movie Title: ', data.get('Title'))
+                print('Movie Year: ', data.get('Year'))
+                print('Movie Rated: ', data.get('Rated'))
+                print('Movie Award: ', data.get('Award'))
+                print('Movie BoxOffice: ', data.get('BoxOffice'))
         except Exception as e:
             print(e)
 
