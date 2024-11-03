cf3da51968f69d439b9cab167e2663aaf9d57e11
umair-tauni <engrumayr@gmail.com>
2024-11-03 17:28:58 +0500

    Add while loop, if else block in while loop and movie search method

--- movieFinder.py
+++ movieFinder.py
@@ -15,11 +15,20 @@ class MovieDescription:
             # print(response.json())
             data = response.json()
             if response.status_code == 200:
-                print('Movie Title: ', data.get('Title'))
-                print('Movie Year: ', data.get('Year'))
-                print('Movie Rated: ', data.get('Rated'))
-                print('Movie Award: ', data.get('Award'))
-                print('Movie BoxOffice: ', data.get('BoxOffice'))
+                if data.get('Response') == 'True':
+                    print('**********************************')
+                    print('Movie Title: ', data.get('Title'))
+                    print('Movie Year: ', data.get('Year'))
+                    print('Movie Rated: ', data.get('Rated'))
+                    print('Movie Award: ', data.get('Award'))
+                    print('Movie BoxOffice: ', data.get('BoxOffice'))
+                    print('Movie Write: ', data.get('Director'))
+                    print('Movie Write: ', data.get('Writer'))
+                    print('**********************************')
+                else:
+                    print('**********************************')
+                    print(f'No movies exist with this {title}')
+                    print('**********************************')
         except Exception as e:
             print(e)
 
@@ -27,6 +36,25 @@ class MovieDescription:
 api_key = '42527132'
 # create object
 movie = MovieDescription(api_key)
-title = input('Enter movie title: ')
-# call method
-movie.searched_movie(title)
+
+# while loop to run program continuosly
+while True:
+    try:
+        title = input('Enter movie title: ')
+        print('Searching movie...........')
+        # call method
+        movie.searched_movie(title)
+        choice = input('Would you like to continue? (y/n): ').lower()
+        if choice == 'y' or choice == 'yes':
+            continue
+        elif choice == 'n' or choice == 'no':
+            print('**************************************')
+            print('Thankyou for using movie finder app..')
+            print('**************************************')
+            break
+        else:
+            print('**************************************')
+            print('Invalid input, please try again!!')
+            print('**************************************')
+    except Exception as e:
+        print(e)
