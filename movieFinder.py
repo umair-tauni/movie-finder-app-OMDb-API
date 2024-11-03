7abde562a526168f6d23fe292605b03ca64026eb
Umair T. <155439061+umair-tauni@users.noreply.github.com>
2024-11-03 18:10:13 +0500

    Movie Finder app functionality (#1)
    * Create movie object, add url to fetch data
    
    * Add Customize Print statements
    
    * Add while loop, if else block in while loop and movie search method
    
    * add comments

--- movieFinder.py
+++ movieFinder.py
@@ -1,1 +1,62 @@
 # movie finder app
+import requests
+
+class MovieDescription:
+    # constructor
+    def __init__(self, api_key):
+        self.api_key = api_key
+    
+    # method
+    def searched_movie(self, title):
+        try:
+            # fetch movie data from the API
+            url = f'http://www.omdbapi.com/?t={title}&apikey={self.api_key}'
+            response = requests.get(url)
+            # print(response.status_code)
+            # print(response.json())
+            data = response.json()
+            if response.status_code == 200:
+                if data.get('Response') == 'True':
+                    # display movie information
+                    print('**********************************')
+                    print('Movie Title: ', data.get('Title'))
+                    print('Movie Year: ', data.get('Year'))
+                    print('Movie Rated: ', data.get('Rated'))
+                    print('Movie Award: ', data.get('Award'))
+                    print('Movie BoxOffice: ', data.get('BoxOffice'))
+                    print('Movie Write: ', data.get('Director'))
+                    print('Movie Write: ', data.get('Writer'))
+                    print('**********************************')
+                else:
+                    print('**********************************')
+                    print(f'No movies exist with this {title}')
+                    print('**********************************')
+        except Exception as e:
+            print(e)
+
+
+api_key = '42527132'
+# create object
+movie = MovieDescription(api_key)
+
+# while loop to run program continuosly
+while True:
+    try:
+        title = input('Enter movie title: ')
+        print('Searching movie...........')
+        # call method
+        movie.searched_movie(title)
+        choice = input('Would you like to continue? (y/n): ').lower()
+        if choice == 'y' or choice == 'yes':
+            continue
+        elif choice == 'n' or choice == 'no':
+            print('**************************************')
+            print('Thankyou for using movie finder app..')
+            print('**************************************')
+            break
+        else:
+            print('**************************************')
+            print('Invalid input, please try again!!')
+            print('**************************************')
+    except Exception as e:
+        print(e)
